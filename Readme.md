a7d445a42030a6d35e1c5b797f9240e26b01b34b
elyacoin <turbobit@closetoya.com>
2019-03-19 18:16:05 +0900

    init

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# ibraries needed : boost >=1.58
+
+How to compile this :
+```
+$ git clone https://github.com/.....
+$ cd laniakey$ mkdir build
+$ cd build
+$ cmake -D STATIC=ON -D CMAKE_BUILD_TYPE=RELEASE ..
+$ PORTABLE=1 make
+```
---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { { 0xBC ,0xDE, 0xED, 0x42 , 0x81, 0x66 , 0x4, 0xf, 0x36, 0x22, 0x0A, 0x49, 0x09, 0xA4, 0x38, 0x6} };
+}
